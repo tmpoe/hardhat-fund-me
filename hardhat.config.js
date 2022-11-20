4787bee5ac1e9ed0007b3ac86b30d400ad8249ce
tmpITK <torok.mark.patrik@gmail.com>
2022-11-20 17:47:26 +0100

    Add solhint and hardhat configs

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,28 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [GOERLI_PRIVATE_KEY],
+      chainId: 5
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      chainId: 31337
+    }
+  },
   solidity: "0.8.17",
-};
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  },
+  gasReporter: {
+    enabled: false,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY
+  }
+}
