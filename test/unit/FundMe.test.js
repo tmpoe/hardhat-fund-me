63a4c620bae0a067332f69f6893847748f68f364
tmpITK <torok.mark.patrik@gmail.com>
2022-11-22 18:03:37 +0100

    add test for correct and incorrect fundings

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -1,10 +1,11 @@
 const { deployments, ethers, getNamedAccounts } = require("hardhat")
-const { assert } = require("chai")
+const { assert, expect } = require("chai")
 
 describe("FundMe", async () => {
   let fundMe
   let deployer
   let mockV3Aggregator
+  const sendValue = ethers.utils.parseEther("1")
   beforeEach(async () => {
     deployer = (await getNamedAccounts()).deployer
     await deployments.fixture(["all"])
@@ -16,6 +17,23 @@ describe("FundMe", async () => {
     it("sets the aggregator correctly", async () => {
       const response = await fundMe.priceFeed()
       assert.equal(response, mockV3Aggregator.address)
+    })
+  })
+
+  describe("fund", async () => {
+    it("fails if you don't send enough eth", async () => {
+      await expect(fundMe.fund()).to.be.revertedWith(
+        "You need to spend more ETH!"
+      )
+    })
+
+    it("correctly updates the funds sent", async () => {
+      await fundMe.fund({ value: sendValue })
+
+      const savedValue = await fundMe.addressToAmountFunded(deployer)
+      console.log("piripocs2")
+
+      assert.equal(savedValue.toString(), sendValue.toString())
     })
   })
 })
